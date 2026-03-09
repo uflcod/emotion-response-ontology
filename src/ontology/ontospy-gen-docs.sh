763d4886a3ab62f87670db13fe6d61c37f14d730
Bill Duncan <wdduncan@users.noreply.github.com>
2026-03-09 17:11:07 -0400

    fiddling with gendocs

--- src/ontology/ontospy-gen-docs.sh
+++ src/ontology/ontospy-gen-docs.sh
@@ -1,2 +1,2 @@
 DOCDIR="${2:-docs}"
-ontospy gendocs $1 --preflabel label --nobrowser --type 3 --theme yeti -o $DOCDIR
+ontospy gendocs $1 --preflabel label --nobrowser --type 2 --theme yeti -o $DOCDIR
